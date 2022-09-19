7fdf9007e55ca0112c0780db8b02433b8f65c617
max <maksximn@gmail.com>
2022-09-19 14:31:00 +0300

    Initial commit

---
+++ src/main/java/com/example/rtf/HelloServlet2.java
@@ -0,0 +1,17 @@
+package com.example.rtf;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+public class HelloServlet2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("2 - " + LocalDateTime.now());
+    }
+}
